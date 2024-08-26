6786eac474e36be48af1e588c6df4908cbe8a5ae
Yury Kamenev <yurkam447@gmail.com>
2024-08-26 19:04:51 +0300

    Added examples for the second roadmap step (registers support)

---
+++ roadmap-2/README.md
@@ -0,0 +1,1 @@
+This folder contains FunC samples using different TVM registers - c0, c1, c2, c3, c4, c5, and c7.
--- run_tsa.sh
+++ run_tsa.sh
@@ -1,1 +1,1 @@
-docker run --platform linux/amd64 -v $1:/project -it --rm explyt/tsa-env:roadmap-1 "${@:2}" 
+docker run --platform linux/amd64 -v $1:/project -it --rm explyt/tsa-env:roadmap-2 "${@:2}"
