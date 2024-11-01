abd0e894d4584257b849c303a3cae41199b5c633
Yury Kamenev <yurkam447@gmail.com>
2024-11-01 16:00:14 +0300

    Updated running script for the fourth roadmap step (first safety properties design)

--- run_tsa.sh
+++ run_tsa.sh
@@ -1,1 +1,1 @@
-docker run --platform linux/amd64 -v $1:/project -it --rm explyt/tsa-env:roadmap-3 "${@:2}"
+docker run --platform linux/amd64 -v $1:/project -it --rm explyt/tsa-env:roadmap-4 "${@:2}"
